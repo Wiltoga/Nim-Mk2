8de34c9d5908d92737c9bfbb70bd204a6b26c2a0
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-12-01 23:11:15 +0100

    improved the help pannel

--- help.c
+++ help.c
@@ -6,15 +6,14 @@
 void help()
 {
     clearScreen();
-       printf(FRONT_WHITE BACK_BLACK);
- printf("Le jeu se deroule sur un plateau de x colonnes et de x lignes.\n"
+    printf(FRONT_BRIGHT_CYAN BACK_BLACK "AIDE :" FRONT_WHITE BACK_BLACK "\n\n");
+    printf("Le jeu se deroule sur un plateau de x colonnes et de x lignes.\n"
            "Il y a un pion dans le coin haut gauche, une arrivee dans le coin bas droit.\n"
            "Il y a des obstacles infranchissables sur le plateau.\n"
-           "A tour de role, le joueur et l'ordinateur realisent des mouvements au pion pour attiendre l'objectif.\n"
+           "A tour de role, le joueur et l'ordinateur realisent des mouvements au pion pour atteindre l'objectif.\n"
            "Les mouvements peuvent se faire vers le bas ou vers la droite, jusqu'a deux cases maximum,\n"
            "et il est impossible d'aller sur un obstacle, ni de le traverser.\n"
-           "Celui qui mettra le pion au point d'arrivee gagne la partie.\n");
-    printf(FRONT_BLACK BACK_BRIGHT_RED "Retour");
+           "Celui qui mettra le pion au point d'arrivee gagne la partie.\n\n");
+    printf(FRONT_BLACK BACK_BRIGHT_RED "Retour" BACK_BLACK "\n");
     while (getArrowPressed() != RETURN);
-    printf(BACK_BLACK);
 }

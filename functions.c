92b0de3e2806e2df0123bc71f93141775397316d
1ner <49456933+1ner@users.noreply.github.com>
2019-12-01 21:36:03 +0100

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,7 +1,6 @@
 #Here you can find the functions used in the game
 
 #Lire_entier is a function that enables you to enter a number which will then be read by the game 
-
   #include <stdio.h>
   #define VMIN 5
   #define VMAX 30
@@ -16,7 +15,6 @@
          if ((entier1>=VMAX) && (entier1<=VMIN))
          printf("The number is not part of the interval.\n");
      }
-      if ((entier1<=VMAX) && (entier1>=VMIN))
-      printf("%d is a part of the interval.", entier1);
-    
+      if ((entier1<=VMAX) && (entier1>=VMIN))  
+        return entier1;
   }

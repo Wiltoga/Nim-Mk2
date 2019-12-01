7d4b442dc49e097dde613c761ca6c5115ecab71d
OxyTom <oxytom8@gmail.com>
2019-12-01 22:36:44 +0100

    smol orthographic mistakes

--- help.c
+++ help.c
@@ -8,10 +8,10 @@ void help()
     clearScreen();
        printf(FRONT_WHITE BACK_BLACK);
  printf("Le jeu se deroule sur un plateau de x colonnes et de x lignes.\n"
-           "Il y a un pion dans le coin haut gauche, une arrivee dans le coin bas droit.\n"
+           "Il y a un pion dans le coin en haut a gauche, une arrivee dans le coin en bas a droite.\n"
            "Il y a des obstacles infranchissables sur le plateau.\n"
            "A tour de role, le joueur et l'ordinateur realisent des mouvements au pion pour attiendre l'objectif.\n"
-           "Les mouvements peuvent se faire vers le bas ou vers la droite, jusqu'a deux cases maximum,\n"
+           "Les mouvements peuvent se faire vers le bas ou vers la droite, jusqu'a deux cases maximum\n"
            "et il est impossible d'aller sur un obstacle, ni de le traverser.\n"
            "Celui qui mettra le pion au point d'arrivee gagne la partie.\n");
     printf(FRONT_BLACK BACK_BRIGHT_RED "Retour");

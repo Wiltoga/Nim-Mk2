1306787f7f3bcd0244117b1f96c7ce62f33821c6
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-12-01 19:22:00 +0100

    added moveCursor function

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 #include "game.h"
-#include "consoleColors.h"
+#include "consoleManagement.h"
 
 int main()
 {
     //http://patorjk.com/software/taag/#p=display&f=Standard&t=Nim
+    printf(FRONT_CYAN"\n");
     printf("\t\t  _   _ _           \n\t\t | \\ | (_)_ __ ___  \n\t\t |  \\| | | '_ ` _ \\ \n\t\t | |\\  | | | | | | |\n\t\t |_| \\_|_|_| |_| |_|\n");
     printf("\t\t______________________");
     startGame();

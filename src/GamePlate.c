55aa5842d3f39b0656e144a76938571abd084c2f
OxyTom <oxytom8@gmail.com>
2019-12-02 21:01:06 +0100

    some correction to comments in GamePlate.c

--- src/GamePlate.c
+++ src/GamePlate.c
@@ -7,16 +7,20 @@
 
 GamePlate* createPlate(GameOptions options)
 {
-    GamePlate* plate = malloc(sizeof(GamePlate));//on créé un plateau
+    GamePlate* plate = malloc(sizeof(GamePlate)); //on créé un plateau
+
     //simple assignation des tailles
     plate->nbColumns = options.ncol;
     plate->nbRows = options.nlig;
     plate->level = options.niveau;
-    plate->cases = malloc(options.nlig*options.ncol*sizeof(Case*));//on créé le tableau
+    plate->cases = malloc(options.nlig*options.ncol*sizeof(Case*)); //on créé le tableau
+
     int i, j;//indexeurs
-    //l'indice de victoire est une valeur qui permet de savoir si une case est gagnante ou pas
+
+    //l'indice de victoire est une valeur qui permet de savoir si une case est gagnante ou pas,
     //étant donné que le plateau suit un shéma particulier (une diagonale de case perdante puis 2 diagonales de cases gagnantes)
     int winningHint = ((options.nlig - 1) + (options.ncol - 1)*2)%3;
+
     for (i = 0;i<options.nlig;i++)
         for (j = 0;j<options.ncol;j++)
         {
@@ -25,18 +29,19 @@ GamePlate* createPlate(GameOptions options)
             tmpCase->banned = false;
             plate->cases[j + i*options.ncol] = tmpCase;
             tmpCase->position = newPosition(j, i);
-            tmpCase->winning = (i + j*2)%3 == winningHint;//test de case gagnante
+            tmpCase->winning = (i + j*2)%3 == winningHint; //test de case gagnante
         }
-        //on génère options.nban cases à bannir
-    for (i = 0;i<options.nban;i++)
+        
+    for (i = 0;i<options.nban;i++) //on génère options.nban cases à bannir
     {
-        //deviens true quand tous les tests sont passés
-        bool nextBan = false;
+        bool nextBan = false; //deviens true quand tous les tests sont passés
+
         while (!nextBan)//tant que les tests sont négatifs
         {
             //on génère les coordonnées de la prochaine case à bannir
             int x = random(0, plate->nbColumns);
             int y = random(0, plate->nbRows);
+
             /*
              * pour les tests suivants, on peut tout mettre dans un seul grand if
              * mais pour des raisons de lisibilité, on segmente
@@ -62,16 +67,17 @@ GamePlate* createPlate(GameOptions options)
                     //s'il s'agit de la case en haut à droite et qu'elle n'a pas de case bannnies dans la diagonale
                     else if (y == 0 && !accessCase(plate, newPosition(x-1, y+1))->banned)
                         nextBan = true;
-                    else if (//sinon si aucune des cases dans sa diagonale (cases interdites) n'est occupée
+                    else if ( //sinon si aucune des cases dans sa diagonale (cases interdites) n'est occupée
                             !accessCase(plate, newPosition(x-1, y+1))->banned &&
                             !accessCase(plate, newPosition(x+1, y-1))->banned)
                         nextBan = true;
                 }
             }
-            if (nextBan)//si la case a passé tous les tests, on la bannie
+            if (nextBan) //si la case a passé tous les tests, on la bannie
                 accessCase(plate, newPosition(x, y))->banned = true;
         }
     }
+    
     for (i = 0;i<plate->nbColumns;i++)
         for (j = 0;j<plate->nbRows;j++)
         {
@@ -94,20 +100,17 @@ GamePlate* createPlate(GameOptions options)
             //si on se trouve une case avant le mur
             else if (i == plate->nbColumns-2)
             {
-                //on récupère la case à droite
-                Case* tmp = accessCase(plate, newPosition(i+1, j));
-                //si elle n'est pas bannie, on l'ajoute aux voisines
-                curr->availableMovements[0] = tmp->banned ? NULL : tmp;
-                //la case suivante est le mur -> on n'en ajoute pas
-                curr->availableMovements[1] = NULL;
+                Case* tmp = accessCase(plate, newPosition(i+1, j)); //on récupère la case à droite
+                curr->availableMovements[0] = tmp->banned ? NULL : tmp; //si elle n'est pas bannie, on l'ajoute aux voisines
+                curr->availableMovements[1] = NULL; //la case suivante est le mur -> on n'en ajoute pas
             }
             else
             {
                 //ici on se trouve ailleurs dans le plateau
-                //on récupère la case à droite
-                Case* tmp = accessCase(plate, newPosition(i+1, j));
-                //si elle n'est pas bannie, on l'ajoute aux voisines
-                curr->availableMovements[0] = tmp->banned ? NULL : tmp;
+                
+                Case* tmp = accessCase(plate, newPosition(i+1, j)); //on récupère la case à droite
+                curr->availableMovements[0] = tmp->banned ? NULL : tmp; //si elle n'est pas bannie, on l'ajoute aux voisines
+
                 //si cette même case n'est pas bannie, on continue de vérifier les déplacements
                 if (!tmp->banned)
                 {
@@ -130,66 +133,61 @@ GamePlate* createPlate(GameOptions options)
             //si on se trouve une case avant le mur
             else if (j == plate->nbRows-2)
             {
-                //on récupère la case en dessous
-                Case* tmp = accessCase(plate, newPosition(i, j+1));
-                //si elle n'est pas bannie, on l'ajoute aux voisines
-                curr->availableMovements[2] = tmp->banned ? NULL : tmp;
-                //la case suivante est le mur -> on n'en ajoute pas
-                curr->availableMovements[3] = NULL;
+                Case* tmp = accessCase(plate, newPosition(i, j+1)); //on récupère la case en dessous
+                curr->availableMovements[2] = tmp->banned ? NULL : tmp; //si elle n'est pas bannie, on l'ajoute aux voisines
+                curr->availableMovements[3] = NULL; //la case suivante est le mur -> on n'en ajoute pas
             }
             else
             {
                 //ici on se trouve ailleurs dans le plateau
-                //on récupère la case en dessous
-                Case* tmp = accessCase(plate, newPosition(i, j+1));
-                //si elle n'est pas bannie, on l'ajoute aux voisines
-                curr->availableMovements[2] = tmp->banned ? NULL : tmp;
+                
+                Case* tmp = accessCase(plate, newPosition(i, j+1)); //on récupère la case en dessous
+                curr->availableMovements[2] = tmp->banned ? NULL : tmp; //si elle n'est pas bannie, on l'ajoute aux voisines
+
                 //si cette même case n'est pas bannie, on continue de vérifier les déplacements
                 if (!tmp->banned)
                 {
-                    //on récupère la case suivante
-                    tmp = accessCase(plate, newPosition(i, j+2));
-                    //si elle n'est pas bannie, on l'ajoute aux voisines
-                    curr->availableMovements[3] = tmp->banned ? NULL : tmp;
+                    tmp = accessCase(plate, newPosition(i, j+2)); //on récupère la case suivante
+                    curr->availableMovements[3] = tmp->banned ? NULL : tmp; //si elle n'est pas bannie, on l'ajoute aux voisines
                 }
                 else
-                    //sinon, on n'ajoute rien au delà de la case bannie
-                    curr->availableMovements[3] = NULL;
+                    curr->availableMovements[3] = NULL; //sinon, on n'ajoute rien au delà de la case bannie
             }
         }
 
 
     return plate;
 }
+
 Case* accessCase(GamePlate* plate, Position pos)
 {
-    //on cherche l'endroit de la case demandée
-    return plate->cases[pos.x + pos.y*plate->nbColumns];
+    return plate->cases[pos.x + pos.y*plate->nbColumns]; //on cherche l'endroit de la case demandée
 }
+
 void freePlate(GamePlate* plate)
 {
-    int i;//indexeur
+    int i; //indexeur
     for (i = 0;i<plate->nbRows*plate->nbColumns;i++)
     //pour chaque element du tableau, on supprime la case
     {
         free(plate->cases[i]->availableMovements);
         free(plate->cases[i]);
     }
-    //on supprime le tableau
-    free(plate->cases);
-    //on supprime le plateau
-    free(plate);
+    
+    free(plate->cases); //on supprime le tableau
+    free(plate); //on supprime le plateau
 }
+
 void renderPlate(GamePlate* plate, Position pawnPos, Position currentSelection)
 {
-    //on efface l'écran
-    clearScreen();
-    //sélection de la couleur d'écriture
-    printf(FRONT_WHITE);
-    //indexs
+    clearScreen(); //on efface l'écran
+    printf(FRONT_WHITE); //sélection de la couleur d'écriture
+    
+    //indexes
     int i, j;
-    //la case sur laquelle se trouve le pion
-    Case* pawnCase = accessCase(plate, newPosition(pawnPos.x, pawnPos.y));
+    
+    Case* pawnCase = accessCase(plate, newPosition(pawnPos.x, pawnPos.y)); //la case sur laquelle se trouve le pion
+    
     for (j = 0;j<plate->nbRows;j++)
     {
         for (i = 0;i<plate->nbColumns;i++)

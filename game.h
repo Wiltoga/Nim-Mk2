1f8a5956d5b9f73f53e1450087effb4572181a4a
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-12-02 17:40:21 +0100

    added IA header

--- game.h
+++ game.h
@@ -2,11 +2,15 @@
 #define DEF_GAME_H
 #define VMIN 5
 #define VMAX 30
+#include "Position.h"
 #include "gameOptions.h"
+#include "GamePlate.h"
 
 //démarre une nouvelle partie de jeu
 void startGame();
 //défini les paramètres de lancement
 GameOptions parameters();
+//donne à l'IA la possibilité de jouer un tour en modifiant la position du pion
+Position IA(GamePlate*, Position);
 
 #endif //DEF_GAME_H

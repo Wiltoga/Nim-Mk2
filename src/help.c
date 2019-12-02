69c83c9961cadcfe8d0a4ce5f834e994fcb3d46d
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2019-12-02 20:50:43 +0100

    help.c comments

--- src/help.c
+++ src/help.c
@@ -5,9 +5,10 @@
 
 void help()
 {
+    //simple menu d'aide
     clearScreen();
     printf(FRONT_BRIGHT_CYAN BACK_BLACK "AIDE :" FRONT_WHITE BACK_BLACK "\n\n");
- printf("Le jeu se deroule sur un plateau de x colonnes et de x lignes.\n"
+    printf("Le jeu se deroule sur un plateau de x colonnes et de x lignes.\n"
            "Il y a un pion dans le coin en haut a gauche, une arrivee dans le coin en bas a droite.\n"
            "Il y a des obstacles infranchissables sur le plateau.\n"
            "A tour de role, le joueur et l'ordinateur realisent des mouvements au pion pour attiendre l'objectif.\n"

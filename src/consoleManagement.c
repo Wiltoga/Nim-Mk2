5bc27e6308e277ab57ebbbcdc23cb24e6aad52be
OxyTom <oxytom8@gmail.com>
2019-12-02 20:55:28 +0100

    some corections to the consoleManagement.c comment

--- src/consoleManagement.c
+++ src/consoleManagement.c
@@ -6,18 +6,18 @@
 int getArrowPressed()
 {
     //inspiré de https://stackoverflow.com/a/10473315
-    int var = getch();//on récupère le code du caractère entré dans la console
-    if (var == 0 || var == 224)//s'il s'agit d'une flèche...
+    int var = getch(); //on récupère le code du caractère entré dans la console
+    if (var == 0 || var == 224) //s'il s'agit d'une flèche...
     {
         switch (getch())
         {
-        case 72://code fèche haut
+        case 72: //code fèche haut
             return UP;
-        case 80://code fèche bas
+        case 80: //code fèche bas
             return DOWN;
-        case 75://code fèche gauche
+        case 75: //code fèche gauche
             return LEFT;
-        case 77://code fèche droite
+        case 77: //code fèche droite
             return RIGHT;
         }
     }
@@ -30,11 +30,9 @@ int getArrowPressed()
 void clearScreen()
 {
     system(
-    #ifdef WIN32
-    //si on est sur Windows
+    #ifdef WIN32 //si on est sur Windows
     "cls");
-    #else
-    //si on est sur Unix
+    #else //si on est sur Unix
     "clear");
     #endif
 }
